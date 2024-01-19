0e624fbf69f66e97e13f44b6e60373a794298635
Ali Nurdin <ali.nrdn14005@gmail.com>
2024-01-19 13:24:47 +0700

    add view kalender jam

--- application/views/pegawai/presensi.php
+++ application/views/pegawai/presensi.php
@@ -5,27 +5,70 @@
 	<title>Capture || Codeigniter</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 </head>
+<style>
+	body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    margin: 50px;
+}
 
+#calendar {
+    display: inline-block;
+}
+
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    border: 1px solid #dddddd;
+    text-align: center;
+    padding: 8px;
+}
+
+th {
+    background-color: #f2f2f2;
+}
+.today {
+    background-color: #4CAF50; /* Warna latar belakang hijau untuk menandai hari ini */
+    color: white;
+    border-radius: 50%;
+}
+#jam {
+    font-size: 20px;
+    color: #333;
+    margin-top: 10px;
+}
+
+
+</style>
 <body>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-6 offset-md-3">
-				<center>
-					<h1>Presensi</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <center>
+                <h1>Presensi</h1>
+                <!-- Tampilan Kalender -->
+				
+                <div id="calendar"></div>
+                <div id="jam"></div>
+                <br>
 
-					<form id="register">
-						<div id="my_camera">
-						</div>
-						<br>
-						<button type="submit" class="btn btn-primary">Absen Sekarang</button>
-						<a class="btn btn-danger" href="<?php echo base_url('pegawai/dashboard') ?>">
-							<i class="fas fa-fw fa-tachometer-alt"></i>
-							<span>Censel</span></a>
-					</form>
-				</center>
-			</div>
-		</div>
-	</div>
+                <form id="register">
+                    <div id="my_camera"></div>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Absen Sekarang</button>
+                    <a class="btn btn-danger" href="<?php echo base_url('pegawai/dashboard') ?>">
+                        <i class="fas fa-fw fa-tachometer-alt"></i>
+                        <span>Censel</span>
+                    </a>
+                </form>
+            </center>
+        </div>
+    </div>
+</div>
+
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
@@ -69,6 +112,64 @@
         });
     });
 });
+document.addEventListener("DOMContentLoaded", function () {
+    createCalendar();
+    updateTime();
+    setInterval(updateTime, 1000); // Update waktu setiap detik
+});
+
+function updateTime() {
+    const jamContainer = document.getElementById("jam");
+    const today = new Date();
+    const currentHours = today.getHours();
+    const currentMinutes = today.getMinutes();
+    const currentSeconds = today.getSeconds();
+
+    jamContainer.innerHTML = `${currentHours}:${currentMinutes < 10 ? '0' : ''}${currentMinutes}:${currentSeconds < 10 ? '0' : ''}${currentSeconds}`;
+}
+
+function createCalendar() {
+	
+    const calendarContainer = document.getElementById("calendar");
+    const today = new Date();
+    const currentMonth = today.getMonth();
+    const currentYear = today.getFullYear();
+    const currentDay = today.getDate();
+
+    const monthNames = [
+        "January", "February", "March", "April",
+        "May", "June", "July", "August",
+        "September", "October", "November", "December"
+    ];
+
+    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+
+    let tableHTML = "<table><tr><th colspan='7'>" + monthNames[currentMonth] + " " + currentYear + "</th></tr>";
+    tableHTML += "<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr><tr>";
+
+    let dayCounter = 1;
+
+    for (let i = 0; i < 42; i++) {
+        if (i >= firstDayOfMonth && dayCounter <= daysInMonth) {
+            // Menandai hari ini
+            const isToday = currentDay === dayCounter && currentMonth === today.getMonth() && currentYear === today.getFullYear();
+            tableHTML += "<td" + (isToday ? " class='today'" : "") + ">" + dayCounter + "</td>";
+            dayCounter++;
+        } else {
+            tableHTML += "<td></td>";
+        }
+
+        if (i % 7 === 6) {
+            tableHTML += "</tr><tr>";
+        }
+    }
+
+    tableHTML += "</tr></table>";
+
+    calendarContainer.innerHTML = tableHTML;
+}
+
 </script>
 </body>
 
--- assets/db/penggajian.sql
+++ assets/db/penggajian.sql
@@ -1,11 +1,3 @@
--- Adminer 4.8.1 MySQL 8.0.30 dump
-
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
-SET NAMES utf8mb4;
 
 DROP TABLE IF EXISTS `data_jabatan`;
 CREATE TABLE `data_jabatan` (
@@ -15,7 +7,7 @@ CREATE TABLE `data_jabatan` (
   `tj_transport` varchar(50) NOT NULL,
   `uang_makan` varchar(50) NOT NULL,
   PRIMARY KEY (`id_jabatan`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
 INSERT INTO `data_jabatan` (`id_jabatan`, `nama_jabatan`, `gaji_pokok`, `tj_transport`, `uang_makan`) VALUES
 (1,	'HRD',	'4000000',	'600000',	'400000'),
@@ -36,7 +28,7 @@ CREATE TABLE `data_kehadiran` (
   `alpha` int NOT NULL,
   `lembur` int NOT NULL,
   PRIMARY KEY (`id_kehadiran`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
 INSERT INTO `data_kehadiran` (`id_kehadiran`, `bulan`, `nik`, `nama_pegawai`, `jenis_kelamin`, `nama_jabatan`, `hadir`, `sakit`, `alpha`, `lembur`) VALUES
 (11,	'012024',	'0987654321',	'Dodi',	'Laki-Laki',	'Staff Marketing',	2,	3,	0,	3),
@@ -56,7 +48,7 @@ CREATE TABLE `data_pegawai` (
   `photo` varchar(100) NOT NULL,
   `hak_akses` int NOT NULL,
   PRIMARY KEY (`id_pegawai`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
 INSERT INTO `data_pegawai` (`id_pegawai`, `nik`, `nama_pegawai`, `username`, `password`, `jenis_kelamin`, `jabatan`, `tanggal_masuk`, `status`, `photo`, `hak_akses`) VALUES
 (1,	'123456789',	'Fauzi',	'fauzi',	'0bd9897bf12294ce35fdc0e21065c8a7',	'Laki-Laki',	'Admin',	'2020-12-26',	'Karyawan Tetap',	'pegawai-210101-a7ca89f5fc.png',	1),
@@ -68,7 +60,7 @@ CREATE TABLE `hak_akses` (
   `keterangan` varchar(50) NOT NULL,
   `hak_akses` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
 INSERT INTO `hak_akses` (`id`, `keterangan`, `hak_akses`) VALUES
 (1,	'admin',	1),
@@ -85,9 +77,9 @@ CREATE TABLE `lembur` (
   `waktu_pulang` time DEFAULT NULL,
   `status_lembur` varchar(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
-  `image_pulang` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `image_pulang` varchar(255)  DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
 INSERT INTO `lembur` (`id`, `id_pegawai`, `nik`, `tanggal`, `tanggal_pulang`, `waktu`, `waktu_pulang`, `status_lembur`, `image`, `image_pulang`) VALUES
 (7,	4,	'09876543211',	'2024-01-14',	NULL,	'21:18:57',	NULL,	'1',	'image_1705267137.jpg',	NULL),
@@ -100,7 +92,7 @@ CREATE TABLE `parameter_gaji` (
   `jml_potongan` int NOT NULL,
   `jenis` varchar(255) NOT NULL DEFAULT 'Potongan',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
 INSERT INTO `parameter_gaji` (`id`, `potongan`, `jml_potongan`, `jenis`) VALUES
 (1,	'Alpha',	50000,	'1'),
@@ -117,9 +109,9 @@ CREATE TABLE `presensi` (
   `waktu_pulang` time DEFAULT NULL,
   `status_absen` varchar(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
-  `image_pulang` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `image_pulang` varchar(255)  DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
 INSERT INTO `presensi` (`id`, `id_pegawai`, `nik`, `tanggal`, `tanggal_pulang`, `waktu`, `waktu_pulang`, `status_absen`, `image`, `image_pulang`) VALUES
 (73,	2,	'0987654321',	'2024-01-14',	'2024-01-14',	'01:53:26',	'20:56:28',	'2',	'image_1705258406.jpg',	'image_1705265788.jpg');
@@ -128,14 +120,13 @@ DROP TABLE IF EXISTS `rekap_absen`;
 CREATE TABLE `rekap_absen` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_pegawai` int DEFAULT NULL,
-  `nik` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nik` varchar(50)  DEFAULT NULL,
   `bulan` varchar(20) DEFAULT NULL,
   `total_absen` int DEFAULT NULL,
   `total_terlambat` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
 INSERT INTO `rekap_absen` (`id`, `id_pegawai`, `nik`, `bulan`, `total_absen`, `total_terlambat`) VALUES
 (5,	NULL,	'0987654321',	'0124',	0,	NULL);
 
--- 2024-01-16 09:36:05
